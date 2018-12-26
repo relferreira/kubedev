c64134427c0170eed19c5079ec8c70bc6e8605d5
Renan Lopes Ferreira <relferreira@gmail.com>
2018-12-26 07:55:59 -0200

    API initialization

---
+++ README.md
@@ -0,0 +1,11 @@
+# KubeDev
+
+Kubernetes Dashboard that helps developers in their everyday usage
+
+## Server
+
+`glide up`
+
+## UI
+
+Work in progress...
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	r := gin.Default()
+	var kubeconfig = flag.String("kubeconfig", filepath.Join(os.Getenv("HOME"), ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+
+	r.GET("/deploy", func(c *gin.Context) {
+		namespace := getNamespaceQueryParam(c)
+
+		deployments, err := clientset.AppsV1beta2().Deployments(namespace).List(metav1.ListOptions{})
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		c.JSON(200, deployments)
+	})
+
+	r.GET("/pods", func(c *gin.Context) {
+		namespace := getNamespaceQueryParam(c)
+
+		pods, err := clientset.CoreV1().Pods(namespace).List(metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+
+		c.JSON(200, pods)
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func getNamespaceQueryParam(c *gin.Context) string {
+	namespace, exists := c.GetQuery("namespace")
+	if !exists {
+		panic("Namespace is required")
+	}
+
+	return namespace
+}
