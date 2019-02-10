821c82a9f69885d33c857c750f8b5dbea222bb9a
Renan Lopes Ferreira <relferreira@gmail.com>
2019-02-10 14:30:14 -0200

    Service info page

--- ui/containers/ServiceInfo.js
+++ ui/containers/ServiceInfo.js
@@ -7,6 +7,10 @@ import {
   getServiceInfo
 } from '../state-management/services-management';
 
+const ServiceType = styled.h3`
+  margin-bottom: 16px;
+`;
+
 export default function ServiceInfo({ namespace, name }) {
   const { response, loading } = getServiceInfo(namespace, name);
 
@@ -21,18 +25,27 @@ export default function ServiceInfo({ namespace, name }) {
   return (
     <div>
       <h1>{metadata.name}</h1>
+      <ServiceType>Type: {spec.type}</ServiceType>
       <h3>IPs</h3>
       <Table>
         <thead>
           <tr>
             <th>Cluster IP</th>
             <th>Public IP</th>
+            <th>Ports</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>{spec.clusterIP}</td>
             <td>{getPublicIP(status.loadBalancer)}</td>
+            <td>
+              {spec.ports.map(portInfo => (
+                <p key={portInfo.port}>
+                  {portInfo.port}/{portInfo.protocol}
+                </p>
+              ))}
+            </td>
           </tr>
         </tbody>
       </Table>
