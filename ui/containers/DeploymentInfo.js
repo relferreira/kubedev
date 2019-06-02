2915706358a376201e5e8631d76048cb9b9cd13a
Renan Lopes Ferreira <relferreira@gmail.com>
2019-06-01 22:03:30 -0300

    Deployment refresh button

--- ui/containers/DeploymentInfo.js
+++ ui/containers/DeploymentInfo.js
@@ -27,6 +27,10 @@ const PodStatusIcon = styled(StatusIcon)`
   height: 12px;
 `;
 
+const RefreshIcon = styled(Icon)`
+  margin-right: 5px;
+`;
+
 export default function DeploymentInfo({ namespace, name, navigate }) {
   const [scale, setScale] = useState(0);
   const { response, loading, query } = getDeployment(
@@ -48,6 +52,8 @@ export default function DeploymentInfo({ namespace, name, navigate }) {
       .catch(err => console.error(err));
   };
 
+  const handleRefresh = () => query();
+
   if (loading) return <div>Loading...</div>;
 
   if (!response) return null;
@@ -62,6 +68,17 @@ export default function DeploymentInfo({ namespace, name, navigate }) {
   return (
     <div>
       <PageHeader title={metadata.name}>
+        <RefreshIcon onClick={handleRefresh}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path fill="none" d="M0 0h24v24H0V0z" />
+            <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" />
+          </svg>
+        </RefreshIcon>
         <Button type="error" onClick={handleDelete}>
           DELETE
         </Button>
