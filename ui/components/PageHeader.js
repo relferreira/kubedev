324981dd12d522d5fdb16400f20d52085eff96a0
Renan Lopes Ferreira <relferreira@gmail.com>
2019-11-20 07:22:30 -0300

    Deployment list as table

--- ui/components/PageHeader.js
+++ ui/components/PageHeader.js
@@ -19,13 +19,14 @@ const PageHeaderContainer = styled.div`
 `;
 
 const RefreshIcon = styled(Icon)`
-  margin-right: 5px;
+  margin-left: 5px;
 `;
 
 const PageHeader = ({
   title,
   showSearch,
   search,
+  showToggleList,
   onSearch,
   onRefresh,
   children
@@ -34,6 +35,11 @@ const PageHeader = ({
     <h1>{title}</h1>
     {showSearch && (
       <Fragment>
+        <Input
+          value={search}
+          placeholder="Search"
+          onChange={event => onSearch(event.target.value)}
+        />
         <RefreshIcon onClick={onRefresh}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -41,15 +47,23 @@ const PageHeader = ({
             height="24"
             viewBox="0 0 24 24"
           >
-            <path fill="none" d="M0 0h24v24H0V0z" />
-            <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" />
+            <path d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z" />
+            <path d="M0 0h24v24H0z" fill="none" />
           </svg>
         </RefreshIcon>
-        <Input
-          value={search}
-          placeholder="Search"
-          onChange={event => onSearch(event.target.value)}
-        />
+        {showToggleList && (
+          <RefreshIcon onClick={onRefresh}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path fill="none" d="M0 0h24v24H0V0z" />
+              <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" />
+            </svg>
+          </RefreshIcon>
+        )}
       </Fragment>
     )}
     {children}
@@ -60,12 +74,14 @@ PageHeader.propTypes = {
   title: PropTypes.string.isRequired,
   showSearch: PropTypes.bool.isRequired,
   search: PropTypes.string,
+  showToggleList: PropTypes.bool,
   onSearch: PropTypes.func,
   onRefresh: PropTypes.func
 };
 
 PageHeader.defaultProps = {
-  showSearch: false
+  showSearch: false,
+  showToggleList: true
 };
 
 export default PageHeader;
--- ui/containers/Deployments.js
+++ ui/containers/Deployments.js
@@ -1,15 +1,21 @@
 import React, { useState, useMemo } from 'react';
 import styled from '@emotion/styled';
+import { Link } from '@reach/router';
 import useSWR from 'swr';
 
 import * as kubectl from '../kubectl';
 import DeployCard from '../components/DeployCard';
 import PageHeader from '../components/PageHeader';
 import { filterSearch } from '../state-management/general-managements';
+import Table from '../components/Table';
 
 const DeployGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(12, 1fr);
+`;
+
+const DeploymentLink = styled(Link)`
+  color: inherit;
 `;
 
 export default function Deployments({ namespace }) {
@@ -32,7 +38,36 @@ export default function Deployments({ namespace }) {
         onSearch={text => setSearch(text)}
         onRefresh={() => revalidate()}
       />
-      <DeployGrid>
+      <Table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Ready</th>
+            <th>Up to Date</th>
+            <th>Available</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items &&
+            items.map(({ metadata, status, spec }) => (
+              <tr key={metadata.name}>
+                <td>
+                  <DeploymentLink to={`${metadata.name}/info`}>
+                    {metadata.name}
+                  </DeploymentLink>
+                </td>
+                <td>
+                  {spec.replicas && `${status.replicas}/${spec.replicas}`}
+                </td>
+                <td>{status.updatedReplicas}</td>
+                <td>{status.availableReplicas}</td>
+                <td>{metadata.creationTimestamp}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+      {/* <DeployGrid>
         {items &&
           items.map(({ metadata, status }) => (
             <DeployCard
@@ -42,7 +77,7 @@ export default function Deployments({ namespace }) {
               // state={status.phase}
             />
           ))}
-      </DeployGrid>
+      </DeployGrid> */}
     </div>
   );
 }
