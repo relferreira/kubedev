d5a62c37ee5714e06157ecca0ca95a8b435421b9
Renan Lopes Ferreira <relferreira@gmail.com>
2019-12-17 07:25:29 -0300

    Table Info for sts page

--- ui/containers/StatefulSets.js
+++ ui/containers/StatefulSets.js
@@ -1,58 +1,39 @@
-import React, { useState, useMemo } from 'react';
-import styled from '@emotion/styled';
+import React from 'react';
 import { Link } from '@reach/router';
-import useSWR from 'swr';
 
-import * as kubectl from '../kubectl';
-import { useConfigContext } from '../state-management/config-management';
-import PageHeader from '../components/PageHeader';
-import { filterSearch } from '../state-management/general-managements';
 import Table from '../components/Table';
+import TableInfo from './TableInfo';
 
 export default function StatefulSets({ namespace }) {
-  const { config } = useConfigContext();
-
-  const [search, setSearch] = useState('');
-  const { data: response, revalidate } = useSWR(
-    [namespace, 'get statefulsets'],
-    kubectl.exec,
-    { suspense: true }
-  );
-
-  const { data } = response || {};
-
-  const items = useMemo(() => filterSearch(data, search), [data, search]);
-
   return (
-    <div>
-      <PageHeader
-        title="StatefulSets"
-        showSearch={true}
-        search={search}
-        onSearch={text => setSearch(text)}
-        onRefresh={() => revalidate()}
-      />
-      <Table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Ready</th>
-            <th>Age</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items &&
-            items.map(({ metadata, status, spec }) => (
-              <tr key={metadata.name}>
-                <td>
-                  <Link to={`${metadata.name}/get`}>{metadata.name}</Link>
-                </td>
-                <td>{`${status.readyReplicas || 0}/${status.replicas}`}</td>
-                <td>{metadata.creationTimestamp}</td>
-              </tr>
-            ))}
-        </tbody>
-      </Table>
-    </div>
+    <TableInfo
+      title="StatefulSets"
+      namespace={namespace}
+      command="get statefulsets"
+    >
+      {items => (
+        <Table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Ready</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items &&
+              items.map(({ metadata, status }) => (
+                <tr key={metadata.name}>
+                  <td>
+                    <Link to={`${metadata.name}/get`}>{metadata.name}</Link>
+                  </td>
+                  <td>{`${status.readyReplicas || 0}/${status.replicas}`}</td>
+                  <td>{metadata.creationTimestamp}</td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      )}
+    </TableInfo>
   );
 }
