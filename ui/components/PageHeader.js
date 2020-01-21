6924b657b45b16ab12bd401f1955516b38d84108
Renan Lopes Ferreira <relferreira@gmail.com>
2020-01-21 07:54:10 -0300

    Tooltips in header icons

--- ui/components/PageHeader.js
+++ ui/components/PageHeader.js
@@ -6,6 +6,7 @@ import Hotkeys from 'react-hot-keys';
 import { useConfigContext } from '../state-management/config-management';
 import Icon from './Icon';
 import Input from './Input';
+import CustomTooltip from './CustomTooltip';
 
 const PageHeaderContainer = styled.div`
   display: flex;
@@ -62,29 +63,33 @@ const PageHeader = ({
               }}
             />
           </Hotkeys>
-          <RefreshIcon onClick={handleListStyleChange}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z" />
-              <path d="M0 0h24v24H0z" fill="none" />
-            </svg>
-          </RefreshIcon>
-          {showToggleList && (
-            <RefreshIcon onClick={onRefresh}>
+          <CustomTooltip label="Change Layout">
+            <RefreshIcon onClick={handleListStyleChange}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
                 height="24"
                 viewBox="0 0 24 24"
               >
-                <path fill="none" d="M0 0h24v24H0V0z" />
-                <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" />
+                <path d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z" />
+                <path d="M0 0h24v24H0z" fill="none" />
               </svg>
             </RefreshIcon>
+          </CustomTooltip>
+          {showToggleList && (
+            <CustomTooltip label="Refresh">
+              <RefreshIcon onClick={onRefresh}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path fill="none" d="M0 0h24v24H0V0z" />
+                  <path d="M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z" />
+                </svg>
+              </RefreshIcon>
+            </CustomTooltip>
           )}
         </Fragment>
       )}
