f2f66b7881b2b0c62e73fd67f76370a6f0ab5648
Renan Lopes Ferreira <relferreira@gmail.com>
2020-07-09 07:39:25 -0300

    Removing unnecessary print

--- utils/staticbox.go
+++ utils/staticbox.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -15,7 +14,6 @@ func Serve(urlPrefix string, box packr.Box) gin.HandlerFunc {
 		fileserver = http.StripPrefix(urlPrefix, fileserver)
 	}
 	return func(c *gin.Context) {
-		fmt.Println(c.Request.URL.Path)
 		if box.Has(c.Request.URL.Path) {
 			fileserver.ServeHTTP(c.Writer, c.Request)
 			c.Abort()
@@ -26,7 +24,6 @@ func Serve(urlPrefix string, box packr.Box) gin.HandlerFunc {
 // RedirectIndex returns a middleware handler that serves redirects to /
 func RedirectIndex() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		fmt.Println(c.Request.URL.Path)
 		c.Redirect(http.StatusMovedPermanently, "/ui")
 		c.Abort()
 	}
