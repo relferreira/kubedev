1293f2192e218ada542a636016add5b1f8cac5f9
Renan Lopes Ferreira <relferreira@gmail.com>
2020-07-30 09:12:56 -0300

    Jobs new table info

--- ui/containers/CronJobs.js
+++ ui/containers/CronJobs.js
@@ -1,65 +1,33 @@
 import React from 'react';
-import styled from '@emotion/styled';
-import { Link } from '@reach/router';
-
-import { useConfigContext } from '../state-management/config-management';
-import CronJobCard from '../components/CronJobCard';
-import Table from '../components/Table';
 import { getActiveJobs } from '../state-management/jobs-management';
-import TableInfo from './TableInfo';
+import NewTableInfo from './NewTableInfo';
 
-const CronJobsGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
-`;
-
-export default function CronJobs({ namespace }) {
-  const { config } = useConfigContext();
-
+export default function CronJobs({ namespace, navigate }) {
   return (
-    <TableInfo title="Cron Jobs" namespace={namespace} command="get cronjobs">
-      {items =>
-        config.listStyle === 'table' ? (
-          <Table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Schedule</th>
-                <th>Suspend</th>
-                <th>Active</th>
-                <th>Last Schedule</th>
-                <th>Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              {items &&
-                items.map(({ metadata, status, spec }) => (
-                  <tr key={metadata.name}>
-                    <td>
-                      <Link to={`${metadata.name}/get`}>{metadata.name}</Link>
-                    </td>
-                    <td>{spec.schedule}</td>
-                    <td>{spec.suspend.toString().toUpperCase()}</td>
-                    <td>{getActiveJobs(status)}</td>
-                    <td>{status.lastScheduleTime}</td>
-                    <td>{metadata.creationTimestamp}</td>
-                  </tr>
-                ))}
-            </tbody>
-          </Table>
-        ) : (
-          <CronJobsGrid>
-            {items &&
-              items.map(({ metadata, spec }) => (
-                <CronJobCard
-                  key={metadata.name}
-                  name={metadata.name}
-                  schedule={spec.schedule}
-                />
-              ))}
-          </CronJobsGrid>
-        )
+    <NewTableInfo
+      title="Cron Jobs"
+      namespace={namespace}
+      command="get cronjobs"
+      navigate={navigate}
+      formatHeader={() => [
+        'Name',
+        'Schedule',
+        'Suspend',
+        'Active',
+        'Last Schedule',
+        'Age'
+      ]}
+      formatItems={items =>
+        items.map(({ metadata, status, spec }) => [
+          metadata.name,
+          spec.schedule,
+          spec.suspend.toString().toUpperCase(),
+          getActiveJobs(status),
+          status.lastScheduleTime,
+          metadata.creationTimestamp
+        ])
       }
-    </TableInfo>
+      dialogItems={[{ value: 'Info', type: 'cronjobs', href: 'get' }]}
+    />
   );
 }
--- ui/containers/Jobs.js
+++ ui/containers/Jobs.js
@@ -1,67 +1,26 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { Link } from '@reach/router';
 
-import { useConfigContext } from '../state-management/config-management';
-import {
-  getCondition,
-  getNumberOfJobs,
-  getCompletedCount
-} from '../state-management/jobs-management';
-import JobCard from '../components/JobCard';
-import Table from '../components/Table';
+import { getCompletedCount } from '../state-management/jobs-management';
+import NewTableInfo from './NewTableInfo';
 
-import TableInfo from './TableInfo';
-
-const JobsGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
-`;
-
-export default function Jobs({ namespace }) {
-  const { config } = useConfigContext();
-
+export default function Jobs({ namespace, navigate }) {
   return (
-    <TableInfo title="Jobs" namespace={namespace} command="get jobs">
-      {items =>
-        config.listStyle === 'table' ? (
-          <Table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Completions</th>
-                <th>Completion</th>
-                <th>Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              {items &&
-                items.map(({ metadata, status, spec }) => (
-                  <tr key={metadata.name}>
-                    <td>
-                      <Link to={`${metadata.name}/get`}>{metadata.name}</Link>
-                    </td>
-                    <td>{getCompletedCount(spec, status)}</td>
-                    <td>{status.completionTime}</td>
-                    <td>{metadata.creationTimestamp}</td>
-                  </tr>
-                ))}
-            </tbody>
-          </Table>
-        ) : (
-          <JobsGrid>
-            {items &&
-              items.map(({ metadata, status }) => (
-                <JobCard
-                  key={metadata.name}
-                  name={metadata.name}
-                  state={getCondition(status)}
-                  number={getNumberOfJobs(status)}
-                />
-              ))}
-          </JobsGrid>
-        )
+    <NewTableInfo
+      title="Jobs"
+      namespace={namespace}
+      command="get jobs"
+      navigate={navigate}
+      formatHeader={() => ['Name', 'Completions', 'Completion', 'Age']}
+      formatItems={items =>
+        items.map(({ metadata, status, spec }) => [
+          metadata.name,
+          getCompletedCount(spec, status),
+          status.completionTime,
+          metadata.creationTimestamp
+        ])
       }
-    </TableInfo>
+      dialogItems={[{ value: 'Info', type: 'jobs', href: 'get' }]}
+    />
   );
 }
