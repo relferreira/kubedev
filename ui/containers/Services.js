951e88dd153d4e67a9e02a8c8c26044848a74d72
Renan Lopes Ferreira <relferreira@gmail.com>
2020-07-30 08:17:51 -0300

    Service page new table info

--- ui/containers/Services.js
+++ ui/containers/Services.js
@@ -1,67 +1,38 @@
 import React from 'react';
-import styled from '@emotion/styled';
-import { Link } from '@reach/router';
 
-import { useConfigContext } from '../state-management/config-management';
-import ServiceCard from '../components/ServiceCard';
 import { getPublicIP, getPorts } from '../state-management/services-management';
+import NewTableInfo from './NewTableInfo';
 
-import Table from '../components/Table';
-import TableInfo from './TableInfo';
-
-const ServicesGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
-`;
-
-export default function Services({ namespace }) {
-  const { config } = useConfigContext();
-
+export default function Services({ namespace, navigate }) {
   return (
-    <TableInfo title="Services" namespace={namespace} command="get services">
-      {items =>
-        config.listStyle === 'table' ? (
-          <Table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Type</th>
-                <th>Cluster IP</th>
-                <th>External IP</th>
-                <th>Port(s)</th>
-                <th>Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              {items &&
-                items.map(({ metadata, status, spec }) => (
-                  <tr key={metadata.name}>
-                    <td>
-                      <Link to={`${metadata.name}/get`}>{metadata.name}</Link>
-                    </td>
-                    <td>{spec.type}</td>
-                    <td>{spec.clusterIP}</td>
-                    <td>{getPublicIP(status.loadBalancer)}</td>
-                    <td>{getPorts(spec.ports)}</td>
-                    <td>{metadata.creationTimestamp}</td>
-                  </tr>
-                ))}
-            </tbody>
-          </Table>
-        ) : (
-          <ServicesGrid>
-            {items &&
-              items.map(({ metadata, spec, status }) => (
-                <ServiceCard
-                  key={metadata.name}
-                  name={metadata.name}
-                  clusterIP={spec.clusterIP}
-                  publicIP={getPublicIP(status.loadBalancer)}
-                />
-              ))}
-          </ServicesGrid>
-        )
+    <NewTableInfo
+      title="Services"
+      namespace={namespace}
+      command="get services"
+      navigate={navigate}
+      formatHeader={() => [
+        'Name',
+        'Type',
+        'Cluster IP',
+        'External IP',
+        'Port(s)',
+        'Age'
+      ]}
+      formatItems={items =>
+        items.map(({ metadata, status, spec }) => [
+          metadata.name,
+          spec.type,
+          spec.clusterIP,
+          getPublicIP(status.loadBalancer),
+          getPorts(spec.ports),
+          metadata.creationTimestamp
+        ])
       }
-    </TableInfo>
+      dialogItems={[
+        { value: 'Port Forward', type: 'services', href: 'get' },
+        { value: 'Edit', type: 'services', href: 'edit' },
+        { value: 'Info', type: 'services', href: 'get' }
+      ]}
+    />
   );
 }
