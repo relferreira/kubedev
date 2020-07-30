0731a152066b73885330bfd4b9f1d49b7e55c000
Renan Lopes Ferreira <relferreira@gmail.com>
2020-07-30 09:36:00 -0300

    Hpa new table info

--- ui/containers/Hpa.js
+++ ui/containers/Hpa.js
@@ -1,46 +1,27 @@
 import React from 'react';
-import { Link } from '@reach/router';
 
-import Table from '../components/Table';
 import { getMetrics } from '../state-management/hpa-management';
-import TableInfo from './TableInfo';
+import NewTableInfo from './NewTableInfo';
 
-export default function Hpa({ namespace }) {
+export default function Hpa({ namespace, navigate }) {
   return (
-    <TableInfo
+    <NewTableInfo
       title="Horizontal Pod Autoscaler"
       namespace={namespace}
       command="get hpa"
-    >
-      {items => (
-        <Table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Targets</th>
-              <th>Min</th>
-              <th>Max</th>
-              <th>Replicas</th>
-              <th>Age</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items &&
-              items.map(({ metadata, status, spec }) => (
-                <tr key={metadata.name}>
-                  <td>
-                    <Link to={`${metadata.name}/get`}>{metadata.name}</Link>
-                  </td>
-                  <td>{getMetrics(metadata)}</td>
-                  <td>{spec.minReplicas}</td>
-                  <td>{spec.maxReplicas}</td>
-                  <td>{status.currentReplicas}</td>
-                  <td>{metadata.creationTimestamp}</td>
-                </tr>
-              ))}
-          </tbody>
-        </Table>
-      )}
-    </TableInfo>
+      navigate={navigate}
+      formatHeader={() => ['Name', 'Targets', 'Min', 'Max', 'Replicas', 'Age']}
+      formatItems={items =>
+        items.map(({ metadata, status, spec }) => [
+          metadata.name,
+          getMetrics(metadata),
+          spec.minReplicas,
+          spec.maxReplicas,
+          status.currentReplicas,
+          metadata.creationTimestamp
+        ])
+      }
+      dialogItems={[{ value: 'Info', type: 'hpa', href: 'get' }]}
+    />
   );
 }
