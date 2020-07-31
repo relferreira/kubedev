ce6a08ae20421112342b79231565cc15ff7e5d90
Renan Lopes Ferreira <relferreira@gmail.com>
2020-07-31 08:38:09 -0300

    secrets new table info

--- ui/containers/Secret.js
+++ ui/containers/Secret.js
@@ -1,37 +1,28 @@
 import React from 'react';
-import { Link } from '@reach/router';
 
-import Table from '../components/Table';
-import TableInfo from './TableInfo';
+import NewTableInfo from './NewTableInfo';
 
-export default function Secret({ namespace }) {
+export default function Secret({ namespace, navigate }) {
   return (
-    <TableInfo title="Secrets" namespace={namespace} command="get secrets">
-      {items => (
-        <Table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Type</th>
-              <th>Data</th>
-              <th>Age</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items &&
-              items.map(({ data, metadata, type }) => (
-                <tr key={metadata.name}>
-                  <td>
-                    <Link to={`${metadata.name}/get`}>{metadata.name}</Link>
-                  </td>
-                  <td>{type}</td>
-                  <td>{data && Object.keys(data).length}</td>
-                  <td>{metadata.creationTimestamp}</td>
-                </tr>
-              ))}
-          </tbody>
-        </Table>
-      )}
-    </TableInfo>
+    <NewTableInfo
+      title="Secrets"
+      namespace={namespace}
+      command="get secrets"
+      navigate={navigate}
+      formatHeader={() => ['Name', 'Type', 'Data', 'Age']}
+      formatItems={items =>
+        items.map(({ metadata, data, type }) => [
+          metadata.name,
+          type,
+          data && Object.keys(data).length,
+          metadata.creationTimestamp
+        ])
+      }
+      dialogItems={[
+        { value: 'Info', type: 'secrets', href: 'get' },
+        { value: 'Edit', type: 'secrets', href: 'edit' },
+        { value: 'Describe', type: 'secrets', href: 'describe' }
+      ]}
+    />
   );
 }
